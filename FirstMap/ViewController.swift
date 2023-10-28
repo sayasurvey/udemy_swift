62802f42f3129dfbf0bf39fa7525b3d56e64087f
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-28 12:44:41 +0900

    Initial Commit

---
+++ FirstMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstMap
+//
+//  Created by 岡本　紗也加 on 2023/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirstMapUITests/FirstMapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FirstMapUITestsLaunchTests.swift
+//  FirstMapUITests
+//
+//  Created by 岡本　紗也加 on 2023/10/28.
+//
+
+import XCTest
+
+final class FirstMapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
